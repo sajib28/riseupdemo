4cc4efd50638d3db80081db86fff618232973810
sajib28 <sajibcse28@gmail.com>
2019-10-27 12:38:50 +0600

    Updates

---
+++ precache-manifest.bccf4b623b2e5e5a581235009c3b8206.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43331a9808fcceb655b3eae8d92577b8",
+    "url": "/riseupdemo/index.html"
+  },
+  {
+    "revision": "0dc403ef757df7e7d093",
+    "url": "/riseupdemo/static/css/main.66e3547d.chunk.css"
+  },
+  {
+    "revision": "acfb4effa6c193b5763f",
+    "url": "/riseupdemo/static/js/2.31dc4e41.chunk.js"
+  },
+  {
+    "revision": "0dc403ef757df7e7d093",
+    "url": "/riseupdemo/static/js/main.be17a955.chunk.js"
+  },
+  {
+    "revision": "c47475a7b26128fae732",
+    "url": "/riseupdemo/static/js/runtime-main.06bdc944.js"
+  },
+  {
+    "revision": "91c3435ee59fe9784844e01004521315",
+    "url": "/riseupdemo/static/media/app.91c3435e.jpg"
+  },
+  {
+    "revision": "38204da8a515d486353cc6b4da8190da",
+    "url": "/riseupdemo/static/media/event.38204da8.jpg"
+  },
+  {
+    "revision": "3165b14bbf3b64fca65829ecde6b800d",
+    "url": "/riseupdemo/static/media/fa-brands-400.3165b14b.eot"
+  },
+  {
+    "revision": "457cb96b6191ed105c4fe8463957f70d",
+    "url": "/riseupdemo/static/media/fa-brands-400.457cb96b.woff"
+  },
+  {
+    "revision": "75af8363f4a38af4de07e056ffa068f9",
+    "url": "/riseupdemo/static/media/fa-brands-400.75af8363.svg"
+  },
+  {
+    "revision": "f2e186cfab4787d4ef6f1bb192aa9a1b",
+    "url": "/riseupdemo/static/media/fa-brands-400.f2e186cf.ttf"
+  },
+  {
+    "revision": "f861a57c52ef711cf807a3eec92c0e17",
+    "url": "/riseupdemo/static/media/fa-brands-400.f861a57c.woff2"
+  },
+  {
+    "revision": "190b0a81aba4077923a625483784d680",
+    "url": "/riseupdemo/static/media/fa-regular-400.190b0a81.svg"
+  },
+  {
+    "revision": "2cd8d991e82712f1a5c5de8ee869ca74",
+    "url": "/riseupdemo/static/media/fa-regular-400.2cd8d991.ttf"
+  },
+  {
+    "revision": "7ab2cb050690f93994c46258d49af5b6",
+    "url": "/riseupdemo/static/media/fa-regular-400.7ab2cb05.woff"
+  },
+  {
+    "revision": "a03df7ab0ffc96cabbfe3dc71c60baaa",
+    "url": "/riseupdemo/static/media/fa-regular-400.a03df7ab.eot"
+  },
+  {
+    "revision": "bd52a727b5449dc3f8195b72c9c58341",
+    "url": "/riseupdemo/static/media/fa-regular-400.bd52a727.woff2"
+  },
+  {
+    "revision": "2cd2be177470d5096992572176bbe76e",
+    "url": "/riseupdemo/static/media/fa-solid-900.2cd2be17.woff2"
+  },
+  {
+    "revision": "5ee32f5c8598e1a63ddf1aad4ffe54f4",
+    "url": "/riseupdemo/static/media/fa-solid-900.5ee32f5c.ttf"
+  },
+  {
+    "revision": "657a469469a2fc38a2901c68a3d56512",
+    "url": "/riseupdemo/static/media/fa-solid-900.657a4694.woff"
+  },
+  {
+    "revision": "a547e21eceadf53602caf057be9ad9fd",
+    "url": "/riseupdemo/static/media/fa-solid-900.a547e21e.eot"
+  },
+  {
+    "revision": "f4e2aa6a10b878a8db2ca3c5c916f30a",
+    "url": "/riseupdemo/static/media/fa-solid-900.f4e2aa6a.svg"
+  },
+  {
+    "revision": "9fe6b1ec9a17be7e73abbbf5ca90be60",
+    "url": "/riseupdemo/static/media/intechnic.9fe6b1ec.mp4"
+  },
+  {
+    "revision": "c6d6ce7eee224b1c728216cae0d0531b",
+    "url": "/riseupdemo/static/media/intechnic.c6d6ce7e.jpg"
+  },
+  {
+    "revision": "fdba0eb5cc076c0a65ba5c6bfe709399",
+    "url": "/riseupdemo/static/media/intro.fdba0eb5.jpg"
+  },
+  {
+    "revision": "447f411de21f1fd366f495b81e7388d6",
+    "url": "/riseupdemo/static/media/national-father.447f411d.png"
+  }
+]);
